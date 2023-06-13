b5110818ab58f5523be434e18ca724bd50784957
omaziarz <omaziarz.pro@gmail.com>
2023-06-13 16:24:34 +0200

    try catch main

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { createHttpServer } from './http';
 import { CSVRow } from './types';
 
 (async function main() {
-  const data = await parseCSV<CSVRow>(join(__dirname, '..', 'data', 'events.csv'));
+  try {
+    const data = await parseCSV<CSVRow>(join(__dirname, '..', 'data', 'events.csv'));
 
-  const server = createHttpServer(data);
-
-  server.listen(3100, () => {
-    console.log('Server listening on port 3100');
-  });
+    const server = createHttpServer(data);
+  
+    server.listen(3100, () => {
+      console.log('Server listening on port 3100');
+    });
+  } catch (e) {
+    console.error(e);
+  }
 })()
